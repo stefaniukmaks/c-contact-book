deac94abc92a526013075adbbb3de9ed7abcab04
medion <medion@localhost.localdomain>
2026-08-19 16:59:40 +0300

    feat: added a save and load contacts function

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct {
     char name[50];
@@ -7,17 +8,20 @@ typedef struct {
 } contact;
 
 void addContact(contact contacts[], int index);
-
+void saveContacts(contact contacts[], int count);
+void loadContacts(contact contacts[], int *count);
 int main() {
     int choice = 0;
     int contactCount = 0;
 
-    contact contacts[2];
+    contact contacts[100];
 
     while (1) {
         printf("\n1. Add contact\n");
         printf("2. Show contacts\n");
-        printf("3. Exit\n");
+        printf("3. Save contacts\n");
+        printf("4. Load contacts\n");
+        printf("5. Exit\n");
         printf("Choose: ");
 
         scanf("%d", &choice);
@@ -42,8 +46,13 @@ int main() {
                            contacts[i].number);
                 }
                 break;
-
             case 3:
+                saveContacts(contacts, contactCount);
+                break;
+            case 4:
+                loadContacts(contacts, &contactCount);
+                break;
+            case 5:
                 return 0;
 
             default:
@@ -70,4 +79,42 @@ void addContact(contact contacts[], int index) {
     getchar();
 
     printf("Contact added!\n");
+}
+
+void saveContacts(contact contacts[], int count){
+    printf("Saving contacts...\n");
+    FILE *pntr = fopen("contacts.txt", "w");
+    if(pntr == NULL){
+        perror("Error opening the file!");
+        return;
+    }
+    for(int i = 0; i < count; i++){
+        fprintf(pntr, "%s\n", contacts[i].name);
+        fprintf(pntr, "%d\n", contacts[i].number);
+    }
+    fclose(pntr);
+}
+
+void loadContacts(contact contacts[], int *count) {
+    FILE *pntr = fopen("contacts.txt", "r");
+
+    if (pntr == NULL) {
+        perror("Error opening the file!");
+        return;
+    }
+
+    char line[100];
+    char num[100];
+
+    while (*count < 100 && fgets(line, sizeof(line), pntr) != NULL) {
+        line[strcspn(line, "\n")] = '\0';
+        strcpy(contacts[*count].name, line);
+        if (fgets(num, sizeof(num), pntr) != NULL) {
+            contacts[*count].number = atoi(num);
+            (*count)++;
+        }
+    }
+
+    fclose(pntr);
+    printf("Contacts loaded successfully!\n");
 }
