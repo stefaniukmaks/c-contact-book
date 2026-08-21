41a43a57bde6e51d4868de9135b1af3e29b30e9e
medion <medion@localhost.localdomain>
2026-08-21 16:05:34 +0300

    feat: complete contact book

--- main.c
+++ main.c
@@ -9,12 +9,15 @@ typedef struct {
 
 void addContact(contact contacts[], int index);
 void saveContacts(contact contacts[], int count);
-void loadContacts(contact contacts[], int *count);
+void loadContacts(contact **contacts, int *count);
+void searchContact(contact contacts[], int count);
+void deleteContact(contact **contacts, int *count);
+void editContact(contact contacts[], int count);
 int main() {
     int choice = 0;
     int contactCount = 0;
 
-    contact contacts[100];
+    contact *contacts = NULL;
 
     while (1) {
         printf("\n1. Add contact\n");
@@ -22,6 +25,9 @@ int main() {
         printf("3. Save contacts\n");
         printf("4. Load contacts\n");
         printf("5. Exit\n");
+        printf("6. Search contact\n");
+        printf("7. Delete contact\n");
+        printf("8. Edit contact\n");
         printf("Choose: ");
 
         scanf("%d", &choice);
@@ -29,14 +35,16 @@ int main() {
 
         switch (choice) {
             case 1:
-                if (contactCount < 100) {
-                    addContact(contacts, contactCount);
-                    contactCount++;
-                } else {
-                    printf("Contact list is full!\n");
+                contact *temp = realloc(contacts, (contactCount + 1) * sizeof(contact));
+                if(temp == NULL){
+                    fprintf(stderr, "Memory allocation failed!\n");
+                    free(contacts);
+                    return 1;
                 }
+                contacts = temp;
+                addContact(contacts, contactCount);
+                contactCount++;
                 break;
-
             case 2:
                 printf("\nContacts:\n");
 
@@ -50,15 +58,25 @@ int main() {
                 saveContacts(contacts, contactCount);
                 break;
             case 4:
-                loadContacts(contacts, &contactCount);
+                loadContacts(&contacts, &contactCount);
                 break;
             case 5:
+                free(contacts);
                 return 0;
-
+            case 6:
+                searchContact(contacts, contactCount);
+                break;
+            case 7:
+                deleteContact(&contacts, &contactCount);
+                break;
+            case 8:
+                editContact(contacts, contactCount);
+                break;
             default:
                 printf("Invalid choice. Please try again.\n");
         }
     }
+    
 
     return 0;
 }
@@ -66,13 +84,9 @@ int main() {
 void addContact(contact contacts[], int index) {
     printf("Enter a name: ");
 
-    fgets(contacts[index].name,
-          sizeof(contacts[index].name),
-          stdin);
+    fgets(contacts[index].name, sizeof(contacts[index].name), stdin);
 
-    contacts[index].name[
-        strcspn(contacts[index].name, "\n")
-    ] = '\0';
+    contacts[index].name[strcspn(contacts[index].name, "\n")] = '\0';
 
     printf("Enter a number: ");
     scanf("%d", &contacts[index].number);
@@ -95,7 +109,8 @@ void saveContacts(contact contacts[], int count){
     fclose(pntr);
 }
 
-void loadContacts(contact contacts[], int *count) {
+void loadContacts(contact **contacts, int *count) {
+    
     FILE *pntr = fopen("contacts.txt", "r");
 
     if (pntr == NULL) {
@@ -106,15 +121,178 @@ void loadContacts(contact contacts[], int *count) {
     char line[100];
     char num[100];
 
+    free(*contacts);
+    *contacts = NULL;
+    *count = 0;
+
     while (*count < 100 && fgets(line, sizeof(line), pntr) != NULL) {
+
+        contact *temp = realloc(*contacts, (*count + 1) * sizeof(contact));
+
+        if(temp == NULL){
+            fprintf(stderr, "Memory allocation failed!\n");
+            fclose(pntr);
+            return;
+        }
+
+        *contacts = temp;
+
         line[strcspn(line, "\n")] = '\0';
-        strcpy(contacts[*count].name, line);
+
+        strcpy((*contacts)[*count].name, line);
+
         if (fgets(num, sizeof(num), pntr) != NULL) {
-            contacts[*count].number = atoi(num);
+            (*contacts)[*count].number = atoi(num);
             (*count)++;
         }
     }
-
+    
     fclose(pntr);
     printf("Contacts loaded successfully!\n");
+}
+
+void searchContact(contact contacts[], int count){
+    if (count == 0) {
+        printf("No contacts available.\n");
+        return;
+    }
+    int choice = 0;
+    int found = 0;
+    printf("1. Search by a name\n");
+    printf("2. Search by a number\n");
+    printf("Choose: ");
+    scanf("%d", &choice);
+    getchar();
+    char searchName[50];
+    if (choice == 1) {
+        printf("\nEnter a name to search: ");
+        fgets(searchName, sizeof(searchName), stdin);
+        searchName[strcspn(searchName, "\n")] = '\0';
+        
+        for(int i = 0; i < count;i++){
+            if(strcmp(contacts[i].name, searchName) == 0){
+                printf("Contact found: Name: %s, Number: %d\n", contacts[i].name, contacts[i].number);
+                printf("\n");
+                found = 1;
+                return;
+            }
+            
+        }
+            if(!found){
+                printf("Contact not found!");
+                printf("\n");
+            }
+    } else if (choice == 2) {
+        printf("Enter a number to search: ");
+        int searchNumber;
+
+        scanf("%d", &searchNumber);
+
+        for(int j = 0; j < count; j++){
+
+            if(contacts[j].number == searchNumber){
+
+                printf("Contact found: Name: %s, Number: %d\n", contacts[j].name, contacts[j].number);
+                found = 1;
+                return;
+            }
+
+        }
+            if(!found){
+                
+                printf("Contact not found!");
+                printf("\n");
+            }
+    } else {
+        printf("Invalid choice.\n");
+        return;
+    }
+}
+
+void deleteContact(contact **contacts, int *count){
+    if (*count == 0) {
+        printf("No contacts available.\n");
+        return;
+        
+    }
+    int choice = 0;
+    printf("\n");
+
+    for(int i = 0; i < *count; i++){
+        printf("%d. %s - %d\n", i + 1, (*contacts)[i].name, (*contacts)[i].number);
+    }
+    printf("Enter contact number to delete: ");
+    scanf("%d", &choice);
+
+    choice--;
+
+    if (choice < 0 || choice >= *count) {
+        printf("Invalid contact number.\n");
+        return;
+    }
+
+    for (int i = choice; i < *count - 1; i++) {
+        (*contacts)[i] = (*contacts)[i + 1];
+    }
+        
+    (*count)--;
+    if (*count == 0) {
+    free(*contacts);
+    *contacts = NULL;
+    printf("Contact deleted successfully!\n");
+    return;
+}
+
+    contact *temp = realloc(*contacts, *count * sizeof(contact));
+
+    if (temp == NULL) {
+    printf("Could not resize contact list.\n");
+    return;
+}
+
+    *contacts = temp;
+}
+
+void editContact(contact contacts[], int count){
+    if (count == 0) {
+        printf("No contacts available.\n");
+        return;
+        
+    }
+    
+
+    int choice = 0;
+    
+    for(int s = 0; s < count; s++){
+        printf("%d. %s - %d\n", s + 1, (contacts)[s].name, (contacts)[s].number);
+    }
+
+    printf("Enter contact number: ");
+    scanf("%d", &choice); 
+
+    getchar();
+    choice--;
+
+    if (choice < 0 || choice >= count) {
+        printf("Invalid contact number.\n");
+        return;
+    }
+
+
+    
+    char changedName[50];
+    int changedNum = 0;
+
+    printf("Enter a new name: ");
+    fgets(changedName, 50, stdin);
+    changedName[strcspn(changedName, "\n")] = '\0';
+
+    printf("Enter new number: ");
+    scanf("%d", &changedNum);
+
+    strcpy(contacts[choice].name, changedName);
+
+    contacts[choice].number = changedNum;
+
+    printf("Successfull!");
 }
